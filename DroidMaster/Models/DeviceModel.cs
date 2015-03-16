215b76823e6ce96efbadfefaae82b5932f0e2b0f
SLaks <Dev@SLaks.net>
2015-03-16 09:01:48 -0400

    Add ProgressModel

--- DroidMaster/Models/DeviceModel.cs
+++ DroidMaster/Models/DeviceModel.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 using DroidMaster.Core;
 
 namespace DroidMaster.Models {
-	///<summary>A wrapper around <see cref="PersistentDevice"/></summary>
+	///<summary>A wrapper around <see cref="PersistentDevice"/>.</summary>
+	///<remarks>The public members of this class form the API  for device scripts.</remarks>
 	public class DeviceModel : INotifyPropertyChanged {
 		internal DeviceModel(PersistentDevice device) {
 			Device = device;
@@ -17,9 +18,9 @@ namespace DroidMaster.Models {
 
 		internal PersistentDevice Device { get; }
 
-		string status;
-		///<summary>Gets or sets a short status message to display in the grid.</summary>
-		public string Status {
+		object status;
+		///<summary>Gets or sets a WPF-bindable short status model object (eg, a string or a <see cref="ProgressModel"/>) to display in the grid.</summary>
+		public object Status {
 			get { return status; }
 			set { status = value; OnPropertyChanged(); }
 		}
@@ -35,5 +36,34 @@ namespace DroidMaster.Models {
 		///<summary>Raises the PropertyChanged event.</summary>
 		///<param name="e">An EventArgs object that provides the event data.</param>
 		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) => PropertyChanged?.Invoke(this, e);
+	}
+
+	///<summary>A WPF-bindable view model that reports the progress of an operation.</summary>
+	public class ProgressModel : IProgress<double>, INotifyPropertyChanged {
+		public void Report(double value) => Progress = value;
+
+		double progress;
+		///<summary>Gets or sets the progress of the operation, between 0 and 1.</summary>
+		public double Progress {
+			get { return progress; }
+			set { progress = value; OnPropertyChanged(); }
+		}
+
+		string description;
+		///<summary>Gets or sets a short description of the operation taking place.</summary>
+		public string Description {
+			get { return description; }
+			set { description = value; OnPropertyChanged(); }
+		}
+
+		///<summary>Occurs when a property value is changed.</summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="name">The name of the property that changed.</param>
+		protected virtual void OnPropertyChanged([CallerMemberName] string name = null) => OnPropertyChanged(new PropertyChangedEventArgs(name));
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="e">An EventArgs object that provides the event data.</param>
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) => PropertyChanged?.Invoke(this, e);
+
 	}
 }
